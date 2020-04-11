f32e8d7dd80df0154161574b819e12ce457a8281
Jayden Kroes <xtra1nfinight@gmail.com>
2020-04-11 10:36:40 +1000

    create data class

---
+++ data.py
@@ -0,0 +1,19 @@
+import random
+
+class Data:
+
+    def __init__(self):
+        self.data = []
+
+    def generate_data(self, type_count, data_length):
+        self.type_count = type_count
+        self.data = []
+        for _ in range(data_length):
+            x = random.random()
+            y = random.random()
+            v = random.randint(0, type_count - 1)
+            self.data.append([[x, y], v])
+        print(self.data)
+
+    def get_data(self):
+        return self.data
--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-print("Hello world")
+import data
+
+new_data = data.Data()
+new_data.generate_data(2, 10)
