b4008b25323620b87142cdc9ac0a7c707bc7547a
Jayden Kroes <xtra1nfinight@gmail.com>
2020-04-25 13:12:41 +1000

    added grid Data

--- data.py
+++ data.py
@@ -20,3 +20,19 @@ class Data:
 
     def get_data(self):
         return self.positions, self.types
+
+class GridData(Data):
+    def __init__(self):
+        super().__init__()
+
+    def generate_data(self, type_count, data_width, data_height):
+        self.type_count = type_count
+        self.positions = []
+        self.types = []
+        for i in range(data_width):
+            for j in range(data_height):
+                x = 1.0/data_width * (i+0.5)
+                y = 1.0/data_height * (j+0.5)
+                v = random.randint(0, type_count - 1)
+                self.positions.append([x,y])
+                self.types.append(v)
